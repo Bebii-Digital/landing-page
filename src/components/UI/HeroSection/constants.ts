a4dbe2e4ba0cfde4cc05839932161051dc855056
adislksn <adi.120140038@student.itera.ac.id>
2025-01-31 16:24:56 +0700

    feat : initial commit template

---
+++ src/components/UI/HeroSection/constants.ts
@@ -0,0 +1,13 @@
+// For desktop
+export const phrases = ['Tingkatkan penjualan', 'produk anda', 'dengan iklan.'];
+export const paragraphPhrases = [
+  "🚀 Maksimalkan Potensi Bisnis Anda",
+  'dengan Strategi Iklan yang Efektif.',
+];
+
+// For mobile
+export const mobilePhrases = ['Tingkatkan penjualan', 'produk anda', 'dengan iklan.'];
+export const mobileParagraphPhrases = [
+  "🚀 Maksimalkan Potensi Bisnis Anda",
+  'dengan Strategi Iklan yang Efektif.',
+];
